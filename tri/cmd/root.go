cf93a24c89d89e70e1c6ceadc6b576000e9696bb
oluwasegun <oilori@pelmorex.com>
2024-02-11 17:57:33 -0500

    Application descrription

--- tri/cmd/root.go
+++ tri/cmd/root.go
@@ -10,18 +10,13 @@ import (
 	"github.com/spf13/cobra"
 )
 
-
-
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "tri",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Tri is a cli todo application",
+	Long: `I seem to be interested in the o programminglanguage and I cam about this tuorial that 
+	teaches how to build cli applications using go. So let's go.
+	Hopefully, this cli application helps you to accomplish your tasks.`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	// Run: func(cmd *cobra.Command, args []string) { },
@@ -47,5 +42,3 @@ func init() {
 	// when this action is called directly.
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
-
-
